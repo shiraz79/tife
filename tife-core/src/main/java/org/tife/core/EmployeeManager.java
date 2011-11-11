01fa3a3c5c361dd87751335f14f23574275360d3
Shiraz Memon <ahmed.shiraz.memon@gmail.com>
2011-11-11 17:08:44 +0100

    some changes

--- tife-core/src/main/java/org/tife/core/EmployeeManager.java
+++ tife-core/src/main/java/org/tife/core/EmployeeManager.java
@@ -8,7 +8,7 @@ import org.tife.core.entity.Employee;
 /**
  * @author a.memon
  *
- * @version 1.0.1-SNAPSHOT
+ * @version 1.0.1
  */
 public interface EmployeeManager {
 	public void addEmployee(Employee e);
